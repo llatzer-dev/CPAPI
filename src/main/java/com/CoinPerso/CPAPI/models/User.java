01822935d3d57d8def5d759831d78bc9bb7a7342
Lázaro <lazaroyaesta@gmail.com>
2022-04-08 11:49:28 +0200

    Add model of User

---
+++ src/main/java/com/CoinPerso/CPAPI/models/User.java
@@ -0,0 +1,28 @@
+package com.CoinPerso.CPAPI.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+    @Field
+    private String firstName;
+    @Field
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return String.format("User[id='%s', firstName='%s', lastName='%s']", id, firstName, lastName);
+    }
+
+}
