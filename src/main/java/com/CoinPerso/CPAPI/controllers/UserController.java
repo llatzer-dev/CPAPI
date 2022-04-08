4b6460218e3fc6aec5bba0e32d1dbc8e6e9b14e9
Lázaro <lazaroyaesta@gmail.com>
2022-04-08 13:00:46 +0200

    Add Controller of User
    Add GET for all users.

---
+++ src/main/java/com/CoinPerso/CPAPI/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.CoinPerso.CPAPI.controllers;
+
+import com.CoinPerso.CPAPI.models.User;
+import com.CoinPerso.CPAPI.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+}
