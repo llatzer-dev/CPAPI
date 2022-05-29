4a2a69cf55b549104d5d8d301767cd766d251fd8
Lázaro <lazaroyaesta@gmail.com>
2022-05-29 13:27:38 +0200

    Update WebSecurityConfig
    The path "/api/user/**" has been added to the configuration.

--- src/main/java/com/CoinPerso/CPAPI/security/WebSecurityConfig.java
+++ src/main/java/com/CoinPerso/CPAPI/security/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
                 .antMatchers("/api/portfolio/**").permitAll()
+                .antMatchers("/api/user/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
